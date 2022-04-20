00412b3001b5da729948d3942149f7a6c69d7496
Quinten Van den Bossche <quinten.vandenbossche@ugent.be>
2022-04-20 11:20:58 +0200

    helper class half

--- Simullll/src/Helper.java
+++ Simullll/src/Helper.java
@@ -1,44 +1,52 @@
-import static java.lang.Math.log;
+import static java.lang.Math.*;
 
 public class Helper {
 
     public double exponentialDistribution(double lambda){
         double j1 = Math.random();
         if(j1 == 0){ j1 = j1 + 0.001;}
-        double j2 = -log(j1)/lambda;
+        double j2 = -Math.log(j1)/lambda;
         return j2;
     }
-    /*
-    double Exponential_distribution(double lambda){
-    float j1 = float (rand()%1000)/1000;
-    if (j1 == 0) j1 += 0.0001;
-    float j2 = -log(j1)/lambda;
-    return j2;
-}
 
-int Poisson_distribution(double lambda){
-    float k, L, j1, j2, j3;
-    int p;
-    j1 = float (rand()%1000)/1000;
-    k = 0;
-    L = exp(-lambda);
-    j3 = 0;
-    do{
-        j2 = L * pow(lambda, k);
-        p = 1;
-        for (int i6 = 0; i6 <= k; i6++){
-            if (i6 == 0)
+    public int poissonDistribution(double lambda){
+        double k, L, j1, j2, j3;
+        int p;
+        j1 = Math.random();
+        k = 0;
+        L = Math.exp(-lambda);
+        j3 = 0;
+        do{
+            j2 = L * (Math.pow(lambda, k));
             p = 1;
-        else
-            p *= i6;
-        }
-        j2 /= p;
-        j3 += j2;
-        k++;
-    } while (j1 >= j3);
+            for(int i6 = 0; i6 <= k; i6++){
+                if(i6 == 0){
+                    p = 1;
+                } else p *= i6;
+            }
+            j2 /= p;
+            j3 += j2;
+            k++;
+        } while (j1 >= j3);
+        return (int) (k-1);
+    }
 
-    return k-1;
-}
+    public double normalDistribution(double mean, double stdev){
+        double v1, v2, t;
+        double x;
+        do{
+            v1 = Math.random()*2;
+            v1 -= 1;
+            v2 = Math.random()*2;
+            v2 -= 1;
+            t = v1*v1 + v2*v2;
+        } while (t >= 1 || t ==0);
+        double multiplier = Math.sqrt(-2*log(t)/t);
+        x = v1 * multiplier * stdev + mean;
+        return x;
+    }
+
+    /*
 
 int Normal_distribution(double mean, double stdev){
     // TO MODEL BASED ON CUMULATIVE DENSITY FUNCTION OF NORMAL DISTRIBUTION BASED ON BOOK OF SHELDON ROSS, Simulation, The polar method, p80.
