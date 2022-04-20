f3068165110f38b05ddfabd9b8dc7e56fde591f7
Quinten Van den Bossche <quinten.vandenbossche@ugent.be>
2022-04-20 11:40:04 +0200

    Helper complete vertaald

--- Simullll/src/Helper.java
+++ Simullll/src/Helper.java
@@ -31,9 +31,9 @@ public class Helper {
         return (int) (k-1);
     }
 
-    public double normalDistribution(double mean, double stdev){
+    public int normalDistribution(double mean, double stdev){
         double v1, v2, t;
-        double x;
+        int x;
         do{
             v1 = Math.random()*2;
             v1 -= 1;
@@ -42,65 +42,39 @@ public class Helper {
             t = v1*v1 + v2*v2;
         } while (t >= 1 || t ==0);
         double multiplier = Math.sqrt(-2*log(t)/t);
-        x = v1 * multiplier * stdev + mean;
+        x = (int) (v1 * multiplier * stdev + mean);
         return x;
     }
-
-    /*
-
-int Normal_distribution(double mean, double stdev){
-    // TO MODEL BASED ON CUMULATIVE DENSITY FUNCTION OF NORMAL DISTRIBUTION BASED ON BOOK OF SHELDON ROSS, Simulation, The polar method, p80.
-
-    float v1, v2, t;
-    int x;
-    do{
-        v1 = float (rand()%1000)*2;
-        v1 /= 1000;
-        v1 -= 1;
-        v2 = float (rand()%1000)*2;
-        v2 /= 1000;
-        v2 -= 1;
-        t=v1*v1+v2*v2;
+    public int bernoulliDistribution(double prob){
+        double j1 = Math.random();
+        if (j1 < prob)
+            return 0;
+        else
+            return 1;
     }
-    while(t>=1||t==0);
-    float multiplier = sqrt(-2*log(t)/t);
-    x = v1 * multiplier * stdev + mean;
-    return x;
-}
 
-int Bernouilli_distribution(double prob){
-    float j1 = rand()%1000/1000;
-    if (j1 < prob)
-        return 0;
-    else
-        return 1;
-}
+    public int uniformDistribution(double a, double b){
+        double j1 = Math.random();
+        return (int) (a + (b-a) * j1);
+    }
 
-int Uniform_distribution(double a, double b){
-    float j1 = rand()%1000/1000;
-    return (int) a + (b-a) * j1;
-}
+    int Triangular_distribution(int a, int b, int c){
+        double mean, stdev,x, L;
 
+        mean = (a+b+c)/3;
+        stdev = (Math.pow(a,2)+Math.pow(b,2)+Math.pow(c,2)-a*b-a*c-b*c)/18;
+        stdev = Math.sqrt(stdev);
+        double j1 = Math.random();
+        x = a;
 
-int Triangular_distribution(int a, int b, int c){
-    float mean, stdev,x, L;
+        do{
+            if (x <= b)
+                L = Math.pow((x-a),2)/((c-a)*(b-a));
+            else
+                L = 1-(Math.pow(c-x,2)/((c-a)*(c-b)));
+            x++;
+        } while (j1 >= L);
 
-    mean = (a+b+c)/3;
-    stdev = (pow(a,2)+pow(b,2)+pow(c,2)-a*b-a*c-b*c)/18;
-    stdev = sqrt(stdev);
-    float j1 = float (rand()%1000)/1000;
-    x = a;
-
-    do{
-        if (x <= b)
-        L = pow((x-a),2)/((c-a)*(b-a));
-    else
-        L = 1-(pow(c-x,2)/((c-a)*(c-b)));
-        x++;
-    } while (j1 >= L);
-
-    return x-1;
-}
-
-     */
+        return (int)x-1;
+    }
 }
