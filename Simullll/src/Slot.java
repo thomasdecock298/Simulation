6a555c3eb13d2780795c227eb3cf0361cdffdcf5
Quinten Van den Bossche <quinten.vandenbossche@ugent.be>
2022-04-20 11:50:58 +0200

    Slot Class get/set

--- Simullll/src/Slot.java
+++ Simullll/src/Slot.java
@@ -1,9 +1,47 @@
 public class Slot {
 
-    static double startTime;
-    static double appTime;
-    static int patientType;
-    static int slotType;
+    private double startTime;
+    private double appTime;
+    private int patientType;
+    private int slotType;
 
+    public Slot(double startTime, double appTime, int patientType, int slotType) {
+        this.startTime = startTime;
+        this.appTime = appTime;
+        this.patientType = patientType;
+        this.slotType = slotType;
+    }
+
+    public double getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(double startTime) {
+        this.startTime = startTime;
+    }
+
+    public double getAppTime() {
+        return appTime;
+    }
+
+    public void setAppTime(double appTime) {
+        this.appTime = appTime;
+    }
+
+    public int getPatientType() {
+        return patientType;
+    }
+
+    public void setPatientType(int patientType) {
+        this.patientType = patientType;
+    }
+
+    public int getSlotType() {
+        return slotType;
+    }
+
+    public void setSlotType(int slotType) {
+        this.slotType = slotType;
+    }
 
 }
